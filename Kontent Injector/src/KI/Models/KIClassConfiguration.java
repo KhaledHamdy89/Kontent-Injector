a6dd87a64095ec57da5d73cd28b0cfd753c2fde5
Khaled Hamdy <khaled_hamdy89@hotmail.com>
2017-03-01 14:05:49 +0000

    Changing access modifiers for class methods

--- Kontent Injector/src/KI/Models/KIClassConfiguration.java
+++ Kontent Injector/src/KI/Models/KIClassConfiguration.java
@@ -19,34 +19,8 @@ public class KIClassConfiguration {
      *
      * @param targetClass The target class
      */
-    public KIClassConfiguration(Class<?> targetClass) {
+    KIClassConfiguration(Class<?> targetClass) {
         this.targetClass = targetClass;
-    }
-
-    /**
-     * Set an alias for the config's class
-     *
-     * @param alias A string defining the target class' alias that will be used in templates
-     */
-    public void setClassAlias(String alias) {
-        this.classAlias = alias;
-    }
-
-    /**
-     * Remove class alias
-     */
-    public void removeClassAlias() {
-        classAlias = null;
-    }
-
-    /**
-     * Add an alias to a method contained inside the target class
-     *
-     * @param methodName  The name of the method
-     * @param methodAlias The method's alias that will be used in templates
-     */
-    public void addMethodAlias(String methodName, String methodAlias) {
-        methodsAliases.put(methodAlias, methodName);
     }
 
     /**
@@ -68,6 +42,32 @@ public class KIClassConfiguration {
     }
 
     /**
+     * Set an alias for the config's class
+     *
+     * @param alias A string defining the target class' alias that will be used in templates
+     */
+    void setClassAlias(String alias) {
+        this.classAlias = alias;
+    }
+
+    /**
+     * Remove class alias
+     */
+    void removeClassAlias() {
+        classAlias = null;
+    }
+
+    /**
+     * Add an alias to a method contained inside the target class
+     *
+     * @param methodName  The name of the method
+     * @param methodAlias The method's alias that will be used in templates
+     */
+    void addMethodAlias(String methodName, String methodAlias) {
+        methodsAliases.put(methodAlias, methodName);
+    }
+
+    /**
      * Gets a method's actual name from its alias
      *
      * @param methodAlias A method's alias that is used in templates
@@ -81,7 +81,7 @@ public class KIClassConfiguration {
      * Remove method Alias
      * @param methodAlias method's alias
      */
-    public void removeMethodAlias(String methodAlias) {
+    void removeMethodAlias(String methodAlias) {
         methodsAliases.remove(methodAlias);
     }
 
