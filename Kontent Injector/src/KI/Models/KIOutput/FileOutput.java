ad8c82318878dd1b6e869a4adf267b6f65983e10
Khaled Hamdy <khaled_hamdy89@hotmail.com>
2017-03-13 17:19:12 +0000

    Updating Javadoc

--- Kontent Injector/src/KI/Models/KIOutput/FileOutput.java
+++ Kontent Injector/src/KI/Models/KIOutput/FileOutput.java
@@ -46,6 +46,30 @@
  * SOFTWARE.
  */
 
+/*
+ * MIT License
+ *
+ * Copyright (c) 2017. Khaled Hamdy
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
 package KI.Models.KIOutput;
 
 import KI.Exceptions.InvalidInputException;
@@ -88,16 +112,26 @@ public class FileOutput implements IKIOutput {
      * @param append   Append to the output file if found
      * @throws IOException           An IOException is thrown if an exception occurred
      *                               while creating the file's print writer
-     * @throws InvalidInputException
+     * @throws InvalidInputException An InvalidInputException is thrown if the output file is not created or unable to
+     *                               create a new output file
      */
     public FileOutput(String filePath, boolean append) throws IOException, InvalidInputException {
         this(new File(filePath), append);
     }
 
+    /**
+     * The File Output class allows the user to send a File for the KI
+     * to write the output to after processing
+     *
+     * @param outputFile Output File
+     * @throws InvalidInputException An InvalidInputException is thrown if the output file is not created or unable to
+     *                               create a new output file
+     * @throws IOException           An IOException is thrown if an exception occurred
+     *                               while creating the file's print writer
+     */
     public FileOutput(File outputFile) throws InvalidInputException, IOException {
         this(outputFile, false);
     }
-
 
     /**
      * The File Output class allows the user to send a File for the KI
